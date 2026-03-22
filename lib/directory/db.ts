688010281e6322846ea30d82d1f94a268e3858b9
NoveR18 <kevtoth22@gmail.com>
2026-03-22 14:56:46 -0400

    Refactor openDB to return IDBDatabase instead of DirectoryDB

--- lib/directory/db.ts
+++ lib/directory/db.ts
@@ -2,9 +2,7 @@ const DB_NAME = "dfas_directory";
 const DB_VERSION = 1;
 const STORE_NAME = "products";
 
-export interface DirectoryDB extends IDBDatabase {}
-
-export const openDB = (): Promise<DirectoryDB> => {
+export const openDB = (): Promise<IDBDatabase> => {
   return new Promise((resolve, reject) => {
     const request = indexedDB.open(DB_NAME, DB_VERSION);
 
@@ -20,7 +18,6 @@ export const openDB = (): Promise<DirectoryDB> => {
           keyPath: "upc",
         });
 
-        // Indexes for future filtering/search
         store.createIndex("brand", "brand", { unique: false });
         store.createIndex("name", "name", { unique: false });
         store.createIndex("type", "type", { unique: false });
