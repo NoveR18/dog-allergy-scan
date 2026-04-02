74110b721515ac42feef3eeba5e5c0c1b46966fb
NoveR18 <kevtoth22@gmail.com>
2026-04-02 16:57:50 -0400

    Update page.tsx

--- app/page.tsx
+++ app/page.tsx
@@ -608,7 +608,7 @@ style={{
   fontSize: 13,
 }}
 >
-  See allergy-safe {product.productCategory} for your pet
+  See allergy-safe {product.productCategory.replaceAll("_", " ")} for your pet
 </button>
               <div style={{ marginTop: 14, borderTop: "1px solid #eee", paddingTop: 14 }}>
                 {verdict === "avoid" && (
